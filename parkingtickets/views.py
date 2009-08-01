e3b06deed3d2efb83a4580f534698a74bbdd32c8
Jamie Matthews <jamie.matthews@gmail.com>
2009-08-01 12:51:38 +0100

    Fixed anonymous session key changing on each request

--- parkingtickets/views.py
+++ parkingtickets/views.py
@@ -8,4 +8,5 @@ def homepage(request):
 	tweets = Tweet.objects.filter(downvotes__lt=5)
 	tweet_count = tweets.count()
 	random_tweet = tweets[randint(0, tweet_count-1)]
+	request.session['parkingticket'] = True
 	return render_to_response('parkingtickets/homepage.html', {'tweet': random_tweet})
