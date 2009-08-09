4d3c0133b152362f1acc6c6898654219fa093603
Jamie Matthews <jamie.matthews@gmail.com>
2009-08-09 12:44:11 +0100

    Fixed mistake in non-existent session key handling

--- tweet_vote/views.py
+++ tweet_vote/views.py
@@ -32,7 +32,7 @@ def vote(request, object_id):
 		
 		if created:
 			response = HttpResponse('success')
-			voted_on = request.session.get(['voted_tweets'], [])
+			voted_on = request.session.get('voted_tweets', [])
 			voted_on.append(tweet.id)
 			request.session['voted_tweets'] = voted_on
 		else:
